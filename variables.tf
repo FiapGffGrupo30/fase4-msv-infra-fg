5975a76d36139977bc9d96408911122cd4559887
Felipe-Leite-de-Souza <felipe_leitesouza@hotmail.com>
2024-01-29 19:49:35 -0300

    ajustes das variaveis do arquivo principal
    Signed-off-by: Felipe-Leite-de-Souza <felipe_leitesouza@hotmail.com>

--- variables.tf
+++ variables.tf
@@ -9,4 +9,17 @@ variable "task_family" {}
 variable "task_cpu" {}
 variable "task_memory" {}
 variable "service_name" {}
-variable "ecr_repository_name" {}
+variable "ecr_repository_name_requests" {}
+variable "ecr_repository_name_payments" {}
+variable "ecr_repository_name_production" {}
+variable "ecr_repository_name_users" {}
+variable "ecr_repository_name_products" {}
+variable "selected_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"]
+}
+variable "ecs_service_requests" {}
+variable "ecs_service_payments" {}
+variable "ecs_service_production" {}
+variable "ecs_service_users" {}
+variable "ecs_service_products" {}
