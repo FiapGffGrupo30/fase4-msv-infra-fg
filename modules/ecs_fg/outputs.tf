26e3cf3e2e32dc115808bd86228e169d09580a55
Felipe-Leite-de-Souza <felipe_leitesouza@hotmail.com>
2024-01-29 19:47:37 -0300

    ajustes no modulo de ecs_fg
    Signed-off-by: Felipe-Leite-de-Souza <felipe_leitesouza@hotmail.com>

--- modules/ecs_fg/outputs.tf
+++ modules/ecs_fg/outputs.tf
@@ -2,10 +2,42 @@ output "ecs_cluster_id" {
   value = aws_ecs_cluster.ecs_cluster.id
 }
 
-output "ecs_task_definition_arn" {
-  value = aws_ecs_task_definition.ecs_task_definition.arn
+output "ecs_task_definition_requests_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition-requests.arn
 }
 
-output "ecs_service_name" {
-  value = aws_ecs_service.ecs_service.name
+output "ecs_task_definition_payments_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition-payments.arn
+}
+
+output "ecs_task_definition_production_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition-production.arn
+}
+
+output "ecs_task_definition_users_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition-users.arn
+}
+
+output "ecs_task_definition_products_arn" {
+  value = aws_ecs_task_definition.ecs_task_definition-products.arn
+}
+
+output "ecs_service_name_requests" {
+  value = aws_ecs_service.ecs_service_requests.name
+}
+
+output "ecs_service_name_payments" {
+  value = aws_ecs_service.ecs_service_payments.name
+}
+
+output "ecs_service_name_production" {
+  value = aws_ecs_service.ecs_service_production.name
+}
+
+output "ecs_service_name_users" {
+  value = aws_ecs_service.ecs_service_users.name
+}
+
+output "ecs_service_name_products" {
+  value = aws_ecs_service.ecs_service_products.name
 }
