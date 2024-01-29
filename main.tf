c881f6293a61bb745ee204581675563570bdc41c
Felipe-Leite-de-Souza <felipe_leitesouza@hotmail.com>
2024-01-29 20:32:53 -0300

    remocao do repositorio e tasks de producao

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
   # OBS 2: Ao criar o S3 deixar abilitado o auto versionamento a
   # cada alteração.
   backend "s3" {
-    bucket = "bucket-s3-gff"
+    bucket = "bucket-cluster-gff"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -32,6 +32,11 @@ module "security-group" {
   vpc_id = module.new-vpc.vpc_id
 }
 
+//module "target-group" {
+//  source = "./modules/target_group"
+//  vpc_id = module.new-vpc.vpc_id
+//}
+
 module "load_balance" {
   source            = "./modules/load_balance"
   vpc_id            = module.new-vpc.vpc_id
@@ -45,7 +50,6 @@ module "ecr_repository" {
   source                         = "./modules/ecr"
   ecr_repository_name_requests   = var.ecr_repository_name_requests
   ecr_repository_name_payments   = var.ecr_repository_name_payments
-  ecr_repository_name_production = var.ecr_repository_name_production
   ecr_repository_name_users      = var.ecr_repository_name_users
   ecr_repository_name_products   = var.ecr_repository_name_products
 }
@@ -61,7 +65,28 @@ module "ecs_fargate" {
   service_name           = var.service_name
   ecs_service_requests   = var.ecs_service_requests
   ecs_service_payments   = var.ecs_service_payments
-  ecs_service_production = var.ecs_service_production
   ecs_service_users      = var.ecs_service_users
   ecs_service_products   = var.ecs_service_products
 }
+
+//module "eks" {
+//  source         = "./modules/eks"
+//  prefix         = var.prefix
+//  vpc_id         = module.new-vpc.vpc_id
+//  cluster_name   = var.cluster_name
+//  retention_days = var.retention_days
+//  subnet_ids     = module.new-vpc.subnet_ids
+//  desired_size   = var.desired_size
+//  max_size       = var.max_size
+//  min_size       = var.min_size
+//}
+//
+//module "cognito" {
+//  source = "./modules/cognito"
+//}
+//
+//module "api_gateway" {
+//  source      = "./modules/api_gateway"
+//  name        = "${var.prefix}-api-gateway"
+//  description = "API Gateway for ${var.prefix}"
+//}
--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -6,10 +6,6 @@ output "ecr_repository_url_payments" {
   value = aws_ecr_repository.repo-payments
 }
 
-output "ecr_repository_url_production" {
-  value = aws_ecr_repository.repo-production
-}
-
 output "ecr_repository_url_users" {
   value = aws_ecr_repository.repo-users
 }
